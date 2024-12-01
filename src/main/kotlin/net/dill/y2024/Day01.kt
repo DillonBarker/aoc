81931754474687c0221cc01fd8e1cf18966079c6
dill <dillon.barker@testevolve.com>
2024-12-01 08:52:01 +0000

    day 1 y 2024

--- src/main/kotlin/net/dill/y2024/Day01.kt
+++ src/main/kotlin/net/dill/y2024/Day01.kt
@@ -7,11 +7,48 @@ class Day01 : Day() {
     private val data by lazy { resourceLines(2024, 1) }
 
     override fun part1(): Int {
-        return 0
+        val firstNums: MutableList<Int> = mutableListOf()
+        val secondNums: MutableList<Int> = mutableListOf()
+        val distances: MutableList<Int> = mutableListOf()
+        data.forEach {
+            val nums = it.split("   ")
+            firstNums.add(nums[0].toInt())
+            secondNums.add(nums[1].toInt())
+        }
+        firstNums.sort()
+        secondNums.sort()
+
+        firstNums.forEachIndexed { index, firstNum ->
+            val secondNum = secondNums[index]
+            if (secondNum >= firstNum) {
+                distances.add(secondNum-firstNum)
+            } else {
+                distances.add(firstNum-secondNum)
+            }
+        }
+        return distances.sum()
     }
 
     override fun part2(): Int {
-        return 0
+        val firstNums: MutableList<Int> = mutableListOf()
+        val secondNums: MutableList<Int> = mutableListOf()
+        val occurences: MutableList<Int> = mutableListOf()
+        data.forEach {
+            val nums = it.split("   ")
+            firstNums.add(nums[0].toInt())
+            secondNums.add(nums[1].toInt())
+        }
+        firstNums.forEach { firstNum ->
+            var count = 0
+            secondNums.forEach { secondNum ->
+                if (firstNum == secondNum) {
+                    count++
+                }
+            }
+            occurences.add(firstNum * count)
+        }
+
+        return occurences.sum()
     }
 
     companion object {
---
+++ src/test/kotlin/net/dill/y2024/Day01Test.kt
@@ -0,0 +1,19 @@
+package net.dill.y2024
+
+import org.junit.jupiter.api.Test
+
+internal class Day01Test {
+    @Test
+    fun `should solve part1`() {
+        val day = Day01()
+
+        assert(day.part1() == 11)
+    }
+
+    @Test
+    fun `should solve part2`() {
+        val day = Day01()
+
+        assert(day.part2() == 31)
+    }
+}
