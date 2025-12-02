10f9e38fbb4c02f3ec15f5bcd01db41c82d5a209
dillon <dillon@dillons-MacBook-Pro.local>
2025-12-02 08:29:42 +0000

    day 2 repeated patterns

--- src/main/kotlin/net/dill/y2025/Day02.kt
+++ src/main/kotlin/net/dill/y2025/Day02.kt
@@ -6,12 +6,78 @@ import net.dill.resourceLines
 open class Day02: Day() {
     override val data by lazy { resourceLines(2025, 2) }
 
-    override fun part1(): Int {
-        return 0
+    override fun part1(): Long {
+        val ranges = data[0].split(",")
+        var invalidIds = 0L
+
+        ranges.forEach { range ->
+            val (start, end) = range.split("-").map { it.toLong() }
+            for (i in start..end) {
+                if (isInvalid(i)) {
+                    invalidIds += i
+                }
+            }
+        }
+
+
+        return invalidIds
     }
 
-    override fun part2(): Int {
-        return 0
+    private fun isInvalid(id: Long): Boolean {
+        val stringedId = id.toString()
+
+        if (stringedId.length % 2 != 0) return false
+        val mid = stringedId.length / 2
+
+        val first = stringedId.take(mid)
+        val second = stringedId.substring(mid)
+
+        return first == second
+    }
+
+    override fun part2(): Long {
+        val ranges = data[0].split(",")
+        var invalidIds = 0L
+
+        ranges.forEach { range ->
+            val (start, end) = range.split("-").map { it.toLong() }
+            for (i in start..end) {
+                if (isInvalid(i)) {
+                    invalidIds += i
+                }
+                else if (isInvalidPart2(i)) {
+                    invalidIds += i
+                }
+            }
+        }
+
+        assert(invalidIds == 70187097315L)
+        return invalidIds
+    }
+
+    private fun isInvalidPart2(id: Long): Boolean {
+        val stringedId = id.toString()
+        return hasRepeatedPattern(stringedId)
+    }
+
+    fun hasRepeatedPattern(string: String, minParts: Int = 3): Boolean {
+        val length = string.length
+
+        // Try all possible n from minParts up to the string length
+        for (n in minParts..length) {
+            if (length % n != 0) continue  // can only split evenly
+
+            val partSize = length / n
+            val first = string.take(partSize)
+
+            val allMatch = (1 until n).all { i ->
+                string.substring(i * partSize, (i + 1) * partSize) == first
+            }
+
+            if (allMatch) return true
+        }
+
+        return false
     }
 
     companion object {
--- src/test/kotlin/net/dill/y2025/Day02Test.kt
+++ src/test/kotlin/net/dill/y2025/Day02Test.kt
@@ -7,13 +7,41 @@ internal class Day02Test {
     fun testSolvePart1() {
         val day = Day02()
 
-        assert(day.part1() == 0)
+        assert(day.part1() == 1227775554L)
     }
 
     @Test
     fun testSolvePart2() {
         val day = Day02()
 
-        assert(day.part2() == 0)
+        assert(day.part2() == 4174379265L)
+    }
+
+    @Test
+    fun testSolvePart2_Twice() {
+        val day = Day02()
+        val id = "12341234"
+        assert(day.hasRepeatedPattern(id))
+    }
+
+    @Test
+    fun testSolvePart2_Thrice() {
+        val day = Day02()
+        val id = "123123123"
+        assert(day.hasRepeatedPattern(id))
+    }
+
+    @Test
+    fun testSolvePart2_Twelve() {
+        val day = Day02()
+        val id = "1212121212"
+        assert(day.hasRepeatedPattern(id))
+    }
+
+    @Test
+    fun testSolvePart2_Seven() {
+        val day = Day02()
+        val id = "1111111"
+        assert(day.hasRepeatedPattern(id))
     }
 }
